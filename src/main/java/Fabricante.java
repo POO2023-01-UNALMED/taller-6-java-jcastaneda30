c3272705c6b838d114c163bc4ec3719f169434a5
jcastaneda30 <jaidercasta30@gmail.com>
2023-03-26 11:09:39 -0500

    2

--- src/main/java/Fabricante.java
+++ src/main/java/Fabricante.java
@@ -1,9 +1,12 @@
 package vehiculos;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Fabricante {
 	private String nombre;
 	private Pais pais;
-	public int cantidadFabrica;
+	public static Map<Fabricante, Integer> vehiculo2 = new HashMap<Fabricante, Integer>();
 	public Fabricante(String nombre, Pais pais) {
 		this.setNombre(nombre);
 		this.setPais(pais);
@@ -14,11 +17,26 @@ public class Fabricante {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public void Vendidos(){
-		cantidadFabrica++;
-	}
-	public int fabricaMayorVentas() {
-		return cantidadFabrica;
+
+	public static Fabricante fabricaMayorVentas() {
+		for(Vehiculo cosa:Vehiculo.vehiculos) {
+			if(vehiculo2.containsKey(cosa.getFabricante())) {
+				int valor = vehiculo2.get(cosa.getFabricante());
+				valor++;
+				vehiculo2.put(cosa.getFabricante(),valor);
+			}else {
+				vehiculo2.put(cosa.getFabricante(), 1);
+			}
+		}
+		int ventas = Integer.MIN_VALUE; // inicializar con el valor mínimo de un entero
+		Fabricante fabricador = null; // inicializar con null
+		for (Map.Entry<Fabricante, Integer> entry : vehiculo2.entrySet()) {
+		    if (entry.getValue() > ventas) {
+		        ventas = entry.getValue();
+		        fabricador = entry.getKey();
+		    }
+		}
+		return fabricador;
 	}
 	public Pais getPais() {
 		return pais;
--- src/main/java/Pais.java
+++ src/main/java/Pais.java
@@ -1,16 +1,35 @@
 package vehiculos;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class Pais {
 	private String nombre;
-	public int cantidadPais;
+	public static Map<Pais, Integer> vehiculo2 = new HashMap<Pais, Integer>();
 	public Pais(String nombre) {
 		this.setNombre(nombre);
 	}
-	public void vender() {
-		cantidadPais++;
-	}
-	public int paisMasVendedor() {
-		return cantidadPais;
+
+	public Pais paisMasVendedor() {
+		for(Vehiculo cosa:Vehiculo.vehiculos) {
+			if(vehiculo2.containsKey(cosa.getFabricante().getPais())) {
+				int valor = vehiculo2.get(cosa.getFabricante().getPais());
+				valor++;
+				vehiculo2.put(cosa.getFabricante().getPais(),valor);
+			}else {
+				vehiculo2.put(cosa.getFabricante().getPais(), 1);
+			}
+		}
+		int ventas = Integer.MIN_VALUE; // inicializar con el valor mínimo de un entero
+		Pais fabricador = null; // inicializar con null
+		for (Entry<Pais, Integer> entry : vehiculo2.entrySet()) {
+		    if (entry.getValue() > ventas) {
+		        ventas = entry.getValue();
+		        fabricador = entry.getKey();
+		    }
+		}
+		return fabricador;
 	}
 	public String getNombre() {
 		return nombre;
